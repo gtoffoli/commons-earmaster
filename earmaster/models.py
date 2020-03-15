535b5c566ef7997a86973327f715398c46abdc65
Giovanni Toffoli <toffoli@linkroma.it>
2020-03-15 18:10:33 +0100

    Version tested with an excerpt of data, waiting for a fix in EarMaster.
    The EM data were exported after setting properly the decimal separator character in Windows.
    The excerpt was produced by removing rows with commas inside the text fields.

--- earmaster/models.py
+++ earmaster/models.py
@@ -1,5 +1,10 @@
 from commons.models import Project
 
 def is_earmaster_project(self):
-    return self.name.count('Music Education')
+    project = self
+    while project.get_type_name() != 'com':
+        if project.name.count('Music Education'):
+            return True
+        project = project.get_parent()
+    return False
 Project.is_earmaster = is_earmaster_project
--- earmaster/views.py
+++ earmaster/views.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 import pyexcel
-import json
+import pytz
 
 from tincan import (
     Statement,
@@ -8,17 +8,21 @@ from tincan import (
     Verb,
     Activity,
     Context,
+    Result,
+    Score,
     LanguageMap,
     ActivityDefinition,
 )
 
-from xapi_client.track.xapi_statements import send_statement
+from xapi_client.utils import xapi_activities, xapi_verbs
+from xapi_client.track.xapi_statements import send_statement, get_language, get_object_id
 
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
 
 from commons.models import Project
 from .forms import ImportResultsForm
@@ -41,10 +45,25 @@ class ImportResults(View):
             data = form.cleaned_data
             project = get_object_or_404(Project, id=data['project'])
             user = data['user']
-            print(project, user)
+
+            timezone = pytz.timezone("Europe/Rome")
+
+            actor = Agent(
+                name=user.get_display_name(),
+                mbox='mailto:%s' % user.email,
+            )
+
+            project_id = get_object_id(request, project)
+
+            activity_type = xapi_activities['assessment']['type']
+            object_language = 'it'
+            verb = Verb(
+                id=xapi_verbs['completed']['id'],
+                display=LanguageMap(**xapi_verbs['completed']['display']),
+            )
+
             file = request.FILES['file']
             filename = file.name
-            print('filename:', filename)
             extension = filename.split(".")[-1]
             content = file.read()
             records = pyexcel.get_records(file_type=extension, file_content=content)
@@ -52,9 +71,76 @@ class ImportResults(View):
             keys = name_dict.keys()
             rows = []
             for record in records:
-                row = [record[key] for key in keys]
-                print(row)
-                rows.append(row)
+                date_time = record['Ora']
+                lesson = record['Lezione']
+                course = record['Corso']
+                activity = record['Attività']
+                duration_seconds = record['Durata (Secondi)']
+                response_seconds = record['Tempo medio di risposta (Secondi)']
+                score_percent = record['Punteggio (%)']
+                questions = record['Domande con risposta']
+                correct_answers = record['Risposte corrette']
+
+                timestamp = datetime.strptime(date_time, "%d/%m/%Y %H.%M")
+                timestamp = timezone.localize(timestamp)
+                
+                object_name = 'EarMaster: {}'.format(activity)
+                object_description = 'Esercizio di {} in lezione EarMaster "{}"'.format(activity, lesson)
+                activity_definition = ActivityDefinition(
+                     name=LanguageMap(**{object_language: object_name}),
+                     description=object_description and LanguageMap(**{object_language: object_description}) or None,
+                     type=activity_type,                                        
+                )
+                course_id = '{}{}'.format(project_id, slugify(course))
+                lesson_id = '{}/{}'.format(course_id, slugify(lesson))
+                object = Activity(
+                    objectType='Activity',
+                    id=lesson_id,
+                    definition=activity_definition,
+                )
+                parent = {
+                   'objectType': 'Activity',
+                   'id': course_id,
+                   'definition': {'type': xapi_activities['course']['type'], 'name': {'it': course}}
+                }
+                grouping = {
+                   'objectType': 'Activity',
+                   'id': project_id,
+                   'definition': {'type': xapi_activities['project']['type'], 'name': LanguageMap(**{get_language(project): project.name})}
+                }
+                context = {
+                    'platform': 'EarMaster',
+                    'context_activities': {
+                        'parent': parent,
+                        'grouping': grouping
+                    }
+                }
+                context = Context(**context)
+
+                score_scaled = float(score_percent.replace('%', ''))/100
+                score_max = questions
+                score_raw = correct_answers
+                score =  {
+                    'min': 0,
+                    'max': score_max,
+                    'raw': score_raw,
+                    'scaled': score_scaled
+                }
+                score = Score(**score)
+                result = {
+                    'duration': duration_seconds,
+                    'score': score
+                }
+                result = Result(**result)
+
+                statement = Statement(
+                    actor=actor,
+                    verb=verb,
+                    object=object,
+                    context=context,
+                    result=result,
+                    timestamp=timestamp
+                )
+                result = send_statement(statement)
             return HttpResponseRedirect('/project/%s/' % project.slug)
-        print('errors:', form.errors)
         return render(request, self.template_name, {'earmaster_import_results_form': form})
